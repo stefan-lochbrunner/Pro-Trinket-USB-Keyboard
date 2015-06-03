1122fbecc7c6f0dac190aa543bad03dfded471a8
stefan-lochbrunner <stefan.lochbrunner@googlemail.com>
2015-06-03 12:04:14 +0200

    updated v3 sketch

--- Pro Trinket sketch/ProTrinketUsbKeyboard_v3/ProTrinketUsbKeyboard_v3.ino
+++ Pro Trinket sketch/ProTrinketUsbKeyboard_v3/ProTrinketUsbKeyboard_v3.ino
@@ -56,50 +56,50 @@ int ledPins[] = {11, 10, 9};
 int leds = sizeof(ledPins)/sizeof(int);
 
 //key sets for different modes
-//usb codes for keypad buttons (4x3) in different modes (2) plus optional modifier
+//usb codes for buttons (4x3) in different modes (3). each button can cause 5 keystrokes + optional modifier. see ProTrinketHidCombo.h for more information.
 //see additional file for codes
-int usb_codes[modes][rows][cols][2] = {
+int usb_codes[modes][rows][cols][6] = {
   //dialpad
-  {
-    {{KEYCODE_1,0},{31,0},{32,0},{33,0}},
-    {{34,0},{35,0},{36,0},{37,0}},
-    {{38,0},{39,0},{10,0},{21,0}}
+  {      // \/ col; {modifier,keycode1,keycode2,keycode3,keycode4,keycode5}
+    {{0,30,0,0,0,0},{0,31,0,0,0,0},{0,32,0,0,0,0},{0,33,0,0,0,0}},    // <-row
+    {{0,34,0,0,0,0},{0,35,0,0,0,0},{0,36,0,0,0,0},{0,37,0,0,0,0}},
+    {{0,38,0,0,0,0},{0,39,0,0,0,0},{0,10,0,0,0,0},{0,21,0,0,0,0}}
   },
   //multimedia
   {
-    {{30,0},{31,0},{32,0},{33,0}},
-    {{34,0},{35,0},{36,0},{37,0}},
-    {{38,0},{10,0},{39,0},{21,0}}
+    {{0,30,0,0,0,0},{0,31,0,0,0,0},{0,32,0,0,0,0},{0,33,0,0,0,0}},
+    {{0,34,0,0,0,0},{0,35,0,0,0,0},{0,36,0,0,0,0},{0,37,0,0,0,0}},
+    {{0,38,0,0,0,0},{0,10,0,0,0,0},{0,39,0,0,0,0},{0,21,0,0,0,0}}
   },
   //eagle
   {
-    {{KEYCODE_F2,KEYCODE_MOD_LEFT_ALT},{KEYCODE_F2,0},{KEYCODE_F6,0},{18,0}},
-    {{KEYCODE_M,KEYCODE_MOD_LEFT_CONTROL},{KEYCODE_G,KEYCODE_MOD_LEFT_CONTROL},{14,0},{KEYCODE_D,KEYCODE_MOD_LEFT_CONTROL}},
-    {{KEYCODE_R,KEYCODE_MOD_LEFT_CONTROL},{10,0},{39,0},{43,KEYCODE_MOD_LEFT_ALT}}
+    {{KEYCODE_MOD_LEFT_ALT,KEYCODE_F2,0,0,0,0},   {0,KEYCODE_F2,0,0,0,0},                      {0,KEYCODE_F6,0,0,0,0}, {5,KEYCODE_R,0,0,0,0}},
+    {{KEYCODE_MOD_LEFT_CONTROL,KEYCODE_M,0,0,0,0},{KEYCODE_MOD_LEFT_CONTROL,KEYCODE_G,0,0,0,0},{3,KEYCODE_A,0,0,0,0},  {KEYCODE_MOD_LEFT_CONTROL,KEYCODE_D,0,0,0,0}},
+    {{KEYCODE_MOD_LEFT_CONTROL,KEYCODE_R,0,0,0,0},{3,KEYCODE_R,0,0,0,0},       {3,KEYCODE_N,0,0,0,0},  {KEYCODE_MOD_LEFT_ALT,43,0,0,0,0}}
   }
 };
 //encoder actions. 1 enc per col, cw & ccw direction (2) plus optional modifier (2)
-int usb_codes_enc[modes][cols][2][2] = {
+int usb_codes_enc[modes][cols][2][6] = {
   //dialpad
   {
-    {{81, 0},{82, 0}},
-    {{30, 0},{31, 0}},
-    {{30, 0},{31, 0}},
-    {{30, 0},{31, 0}}
+    {{0,81,0,0,0,0},{0,82,0,0,0,0}},
+    {{0,30,0,0,0,0},{0,31,0,0,0,0}},
+    {{0,30,0,0,0,0},{0,31,0,0,0,0}},
+    {{0,30,0,0,0,0},{0,31,0,0,0,0}}
   },
   //multimedia
   {
-    {{233, 0},{234, 0}},
-    {{81, 0},{82, 0}},
-    {{81, 0},{82, 0}},
-    {{81, 0},{82, 0}}
+    {{0,233,0,0,0,0},{0,234,0,0,0,0}},
+    {{0,81,0,0,0,0},{0,82,0,0,0,0}},
+    {{0,81,0,0,0,0},{0,82,0,0,0,0}},
+    {{0,81,0,0,0,0},{0,82,0,0,0,0}}
   },
   //eagle
   {
-    {{KEYCODE_F3, 0},{KEYCODE_F4, 0}},
-    {{KEYCODE_Y, KEYCODE_MOD_LEFT_CONTROL},{KEYCODE_Z, KEYCODE_MOD_LEFT_CONTROL}},
-    {{81, 0},{82, 0}},
-    {{81, 0},{82, 0}}
+    {{0,KEYCODE_F3,0,0,0,0},                      {0,KEYCODE_F4,0,0,0,0}},
+    {{0,KEYCODE_MOD_LEFT_CONTROL,KEYCODE_Y,0,0,0},{0,KEYCODE_MOD_LEFT_CONTROL,KEYCODE_Z,0,0,0}},
+    {{0,81,0,0,0,0},                              {0,82,0,0,0,0}},
+    {{0,81,0,0,0,0},                              {0,82,0,0,0,0}}
   }
 };
 
@@ -126,7 +126,6 @@ void setup(){
     delay(200);
     digitalWrite(ledPins[i], LOW);
   }
-  delay(500);
   
   //USB
   TIMSK0&=!(1<<TOIE0);
@@ -143,99 +142,93 @@ void loop(){
   //poll USB
   TrinketHidCombo.poll();
   
-  //break criteria for while loop (to service USB)
-  int count = 0;
   //poll inputs
-  while(count < 5){
-    count ++;
-    
-    //poll mode button
-    if(digitalRead(modeBtn) == 0 && modeBtnPrev == 1){
-      modeBtnPrev = 0;
-      mode += 1;
-      if(mode >= modes){
-        mode = 0;
-      }
-      //change LEDs
-      //turn off all LEDs
-      for(int i=0; i<leds; i++){
-        digitalWrite(ledPins[i], LOW);
-      }
-      //turn on current LED
-      digitalWrite(ledPins[mode], HIGH);
-      
-      //TrinketHidCombo.pressMultimediaKey(MMKEY_MUTE);
+  
+  //poll mode button
+  if(digitalRead(modeBtn) == 0 && modeBtnPrev == 1){
+    modeBtnPrev = 0;
+    mode += 1;
+    if(mode >= modes){
+      mode = 0;
     }
-    else if (digitalRead(modeBtn) == 1 && modeBtnPrev == 0){
-      modeBtnPrev = 1;
+    //change LEDs
+    //turn off all LEDs
+    for(int i=0; i<leds; i++){
+      digitalWrite(ledPins[i], LOW);
     }
-    
-    //cycle through cols
-    for(int i=0; i<cols; i++){
-      //enable col (active low)
-      digitalWrite(colPins[i], LOW);
-
-      //poll rows
-      for(int j=0; j<rows; j++){
-        //read state of current btn
-        int but = digitalRead(rowPins[j]);    //Button Under Test
-        
-        //btn was just pressed (0), which means prev state was 1 (unpressed)
-        if(but == 0 && key_prev[j][i] == 1){
-          //set new prev state
-          key_prev[j][i] = 0;
-          
-          //execute keystroke
-          TrinketHidCombo.pressKey(usb_codes[mode][j][i][1], usb_codes[mode][j][i][0]);
-          TrinketHidCombo.pressKey(0, 0);
-        }
-        //key release
-        else if(but == 1 && key_prev[j][i] == 0){
-          //set new prev state
-          key_prev[j][i] = 1;
-        }
-      }
-      // /poll rows
-
-      //poll encoder
-      int A = digitalRead(encPins[0]);
-      int B = digitalRead(encPins[1]);
-      //detect rotation
-      if(A == 0 && enc_prev[i] == 1){
-        //change prev state
-        enc_prev[i] = 0;
-        
-        //determine rotation direction (cw/ccw)
-        if(B == 1){
-          //cw rotation ...orwhatever? 
-          
-          //execute keystroke
-          TrinketHidCombo.pressKey(usb_codes_enc[mode][i][0][1], usb_codes_enc[mode][i][0][0]);
-          TrinketHidCombo.pressKey(0, 0);
-        }
-        else if(B == 0){
-          //ccw rotation ...orwhatever?
-          
-          //execute keystroke
-          TrinketHidCombo.pressKey(usb_codes_enc[mode][i][1][1], usb_codes_enc[mode][i][1][0]);
-          TrinketHidCombo.pressKey(0, 0);
-        }
-      }
-      else if(A == 1 && B == 1){
-        //no input on encoder pins
-        enc_prev[i] = 1;
-      }
-      // /poll encoder
-
-      //disable col
-      digitalWrite(colPins[i], HIGH);
-      
-      delayMicroseconds(250);    //software debounce
-    }
-    // /cycle through cols
+    //turn on current LED
+    digitalWrite(ledPins[mode], HIGH);
   }
+  else if (digitalRead(modeBtn) == 1 && modeBtnPrev == 0){
+    modeBtnPrev = 1;
+  }
+  
+  //cycle through cols
+  for(int i=0; i<cols; i++){
+    //enable col (active low)
+    digitalWrite(colPins[i], LOW);
+
+    //poll rows
+    for(int j=0; j<rows; j++){
+      //read state of current btn
+      int but = digitalRead(rowPins[j]);    //Button Under Test
+      
+      //btn was just pressed (0), which means prev state was 1 (unpressed)
+      if(but == 0 && key_prev[j][i] == 1){
+        //set new prev state
+        key_prev[j][i] = 0;
+        
+        //execute keystroke
+        TrinketHidCombo.pressKey(usb_codes[mode][j][i][0], usb_codes[mode][j][i][1]);
+        TrinketHidCombo.pressKey(0, 0);
+      }
+      //key release
+      else if(but == 1 && key_prev[j][i] == 0){
+        //set new prev state
+        key_prev[j][i] = 1;
+      }
+    }
+    // /poll rows
+
+    //poll encoder
+    int A = digitalRead(encPins[0]);
+    int B = digitalRead(encPins[1]);
+    //detect rotation
+    if(A == 0 && enc_prev[i] == 1){
+      //change prev state
+      enc_prev[i] = 0;
+      
+      //determine rotation direction (cw/ccw)
+      if(B == 1){
+        //cw rotation ...orwhatever? 
+        
+        //execute keystroke
+        TrinketHidCombo.pressKey(usb_codes_enc[mode][i][0][0], usb_codes_enc[mode][i][0][1]);
+        TrinketHidCombo.pressKey(0, 0);
+      }
+      else if(B == 0){
+        //ccw rotation ...orwhatever?
+        
+        //execute keystroke
+        TrinketHidCombo.pressKey(usb_codes_enc[mode][i][1][0], usb_codes_enc[mode][i][1][1]);
+        TrinketHidCombo.pressKey(0, 0);
+      }
+    }
+    else if(A == 1 && B == 1){
+      //no input on encoder pins
+      enc_prev[i] = 1;
+    }
+    // /poll encoder
+
+    //disable col
+    digitalWrite(colPins[i], HIGH);
+    
+    delayMicroseconds(250);    //software debounce
+  }
+  // /cycle through cols
 }
 
+//differentiate between keycode and multimedia keycode
 void executeKeystroke(){
   
 }
